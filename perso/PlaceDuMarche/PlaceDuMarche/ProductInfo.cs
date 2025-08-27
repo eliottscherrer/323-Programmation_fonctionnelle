ad236647312346fbca91b2de1fb39df07870bb19
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2025-08-27 10:00:39 +0200

    refactor(PlaceDuMarché): add ProductInfo and SellerInfo structs for clarity when handling data

---
+++ perso/PlaceDuMarche/PlaceDuMarche/ProductInfo.cs
@@ -0,0 +1,10 @@
+namespace PlaceDuMarche
+{
+    public struct ProductInfo
+    {
+        public string Name;
+        public int Quantity;
+        public string Seller;
+        public int StandId;
+    }
+}
---
+++ perso/PlaceDuMarche/PlaceDuMarche/SellerInfo.cs
@@ -0,0 +1,9 @@
+namespace PlaceDuMarche
+{
+    public struct SellerInfo
+    {
+        public string Seller;
+        public int StandId;
+        public int Quantity;
+    }
+}
