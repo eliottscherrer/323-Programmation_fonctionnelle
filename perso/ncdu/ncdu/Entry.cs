47622375f1def7e205aeca49ee78fa49db2c1985
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2025-08-27 10:40:10 +0200

    feat(ncdu): add Entry class for file metadata

---
+++ perso/ncdu/ncdu/Entry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ncdu
+{
+    public class Entry
+    {
+        public string Name { get; set; }
+        public long Size { get; set; }
+    }
+}
