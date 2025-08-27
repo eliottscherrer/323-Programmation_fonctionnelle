bb24c3a4cde8fd3100d7e974af0bd244a1d28dbe
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2025-08-27 09:29:08 +0200

    feat(PlaceDuMarché): add debug PrintSheet function

--- perso/PlaceDuMarche/PlaceDuMarche/Program.cs
+++ perso/PlaceDuMarche/PlaceDuMarche/Program.cs
@@ -14,9 +14,43 @@ namespace PlaceDuMarche
 {
     public class Program
     {
-        static void Main(string[] args)
-        {
+        const string filePath = @"C:\Users\pg13qnw\Documents\GitHub\323-Programmation_fonctionnelle\exos\marché\Place du marché.xlsx";
 
+        static void Main()
+        {
+            PrintSheet("Produits");
+
+            Console.ReadLine();
+        }
+
+        static void PrintSheet(string sheetName)
+        {
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                IWorkbook workbook = new XSSFWorkbook(fileStream);
+
+                ISheet sheet = workbook.GetSheet(sheetName);
+
+                for (int rowIndex = 0; rowIndex <= sheet.LastRowNum; rowIndex++)
+                {
+                    IRow row = sheet.GetRow(rowIndex);
+                    if (row != null)
+                    {
+                        for (int cellIndex = 0; cellIndex < row.LastCellNum; cellIndex++)
+                        {
+                            ICell cell = row.GetCell(cellIndex);
+                            if (cell != null)
+                            {
+                                Console.Write(cell.ToString() + "\t");
+                            }
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
