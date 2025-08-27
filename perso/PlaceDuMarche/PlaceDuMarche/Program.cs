2be0710e6bcc6e1e7adb8f2fe4bd5103c0455e74
Eliott Scherrer <146830321+eliottscherrer@users.noreply.github.com>
2025-08-27 09:26:05 +0200

    chore(PlaceDuMarché): add initial project

---
+++ perso/PlaceDuMarche/PlaceDuMarche/Program.cs
@@ -0,0 +1,22 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.Formula.Functions;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace PlaceDuMarche
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
